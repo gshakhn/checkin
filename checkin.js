358a8790cdd2d89e7335fb05b230289eb532514d
George Shakhnazaryan <gshakhn@gmail.com>
2013-05-27 01:07:04 -0500

    Support no checkins in data file.

--- checkin.js
+++ checkin.js
@@ -68,18 +68,20 @@ if (Meteor.isServer) {
       }
     });
 
-    Meteor.settings.checkins.forEach(function(checkinData) {
-      var team = Teams.findOne({name: checkinData.teamName});
-      if (Checkins.findOne({day: checkinData.day, teamId: team._id, description: checkinData.description}) === undefined) {
-	console.log("Adding checkin for team " + team.name + " on " + (new Date(checkinData.day)).toLocaleDateString());
-        Checkins.insert({
-	  teamId: team._id,
-	  description: checkinData.description,
-	  day: checkinData.day,
-	  createdDate: (new Date()).toISOString()
-	});
-      }
-    });
+    if (Meteor.settings.checkins !== undefined) {
+      Meteor.settings.checkins.forEach(function(checkinData) {
+        var team = Teams.findOne({name: checkinData.teamName});
+        if (Checkins.findOne({day: checkinData.day, teamId: team._id, description: checkinData.description}) === undefined) {
+	  console.log("Adding checkin for team " + team.name + " on " + (new Date(checkinData.day)).toLocaleDateString());
+          Checkins.insert({
+            teamId: team._id,
+	    description: checkinData.description,
+	    day: checkinData.day,
+	    createdDate: (new Date()).toISOString()
+	  });
+        }
+      });
+    }
   });
 }
 
